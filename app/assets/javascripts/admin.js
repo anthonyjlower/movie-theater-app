71161561b2f74796bcfe019dc2acc6d66d6b6e74
Anthony <anthonyjlower@gmail.com>
2018-04-11 04:12:42 -0500

    Fixed date picker issues

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,16 +1,15 @@
-window.onload = function(){
+document.addEventListener('turbolinks:load', () => {
 	dates = document.getElementsByClassName('date')
 
 	for (let i = 0; i < dates.length; i++) {
 		dates[i].addEventListener('click', selectDate)
 	}
-};
+})
 
 function selectDate(e) {
 	const day = e.currentTarget.innerText
 	const span = document.getElementsByTagName('span')
 	const active = document.getElementsByClassName(day)
-	console.log(day)
 	
 	
 	for (let i = 0; i < span.length; i++) {
